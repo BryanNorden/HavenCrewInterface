31a8ee00853f47b77fd6c99d407494552c21f58c
Bryan Norden <bryan@nedronlabs.com>
2025-05-20 01:35:32 -0700

    Initial Commit

---
+++ HavenVitals/HavenVitalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HavenVitalsApp.swift
+//  HavenVitals
+//
+//  Created by Bryan Norden on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct HavenVitalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
